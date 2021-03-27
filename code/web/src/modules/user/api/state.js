89ccd77af2d76f983f06dd6ed63e02751874efd6
Thao Ma <MaThao@gmail.com>
2021-03-27 15:53:59 -0600

    Add annotations in state.js

--- code/web/src/modules/user/api/state.js
+++ code/web/src/modules/user/api/state.js
@@ -2,7 +2,7 @@
 import { isEmpty } from '../../../setup/helpers'
 import { SET_USER, LOGIN_REQUEST, LOGIN_RESPONSE, LOGOUT } from './actions'
 
-// Initial State
+// Initial State (setting what will be an automatic state)
 export const userInitialState = {
   error: null,
   isLoading: false,
@@ -11,6 +11,7 @@ export const userInitialState = {
 }
 
 // State
+// Here below is our action creators which are functionx that returns an object. There is an action type that is assigned to a case set in a switch statement where if the action.type matches they state then it will return that specific object.
 export default (state = userInitialState, action) => {
   switch (action.type) {
     case SET_USER:
