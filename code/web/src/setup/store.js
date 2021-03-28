0630966b55de2176abf9a62cf8b8cf38516918a1
Luke Mason <lukjmason@gmail.com>
2021-03-28 09:57:56 -0600

    Add annotations for store.js

--- code/web/src/setup/store.js
+++ code/web/src/setup/store.js
@@ -12,6 +12,8 @@ import * as subscription from '../modules/subscription/api/state'
 import * as crate from '../modules/crate/api/state'
 
 // App Reducer
+// This is where all reducer methods are combined into one another. The one that our track focuses on
+// specifically will be the user reducer, and possibly the subscriptions one as well.
 const appReducer = combineReducers({
   common,
   user,
@@ -37,11 +39,17 @@ if (typeof window !== 'undefined') {
 }
 
 // Store
+// This is where the store is being created that holds the hole state of the application. The composeWithDevTools
+// method is used here so that we can access the Redux dev tools in the browser. The store is comprised
+// of all the reducer methods (rootReducer variable), and with the initial state of the application.
 export const store = createStore(
   rootReducer,
   initialState,
 
   composeWithDevTools(
+// This applyMiddleware method is something that is new to me and will take further research to fully understand.
+// I know it has something to do with how actions are being dispatched to different components
+// through out the application.
     applyMiddleware(thunk),
   )
 )
