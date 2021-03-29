43aae622b99879e07604401e71fef2416674f638
Saundra Catalina <saundracatalina@gmail.com>
2021-03-28 22:06:56 -0600

    Add annotations for additions to user model

--- code/api/src/modules/user/model.js
+++ code/api/src/modules/user/model.js
@@ -1,3 +1,13 @@
+// Define additional columns that will be added with migrations
+// to the Users table to this file and their DataTypes in the
+// same syntax that the original columns are already listed.
+
+// Near the bottom of file add another User.associate to detail
+// the new association that a User.hasMany deliveries
+
+// All of this will help query the database and give access to the FE
+// to display this data 
+
 'use strict'
 
 // User
