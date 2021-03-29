b43bf3ce7f147075ba615b6b1294b4f758fd726a
Saundra Catalina <saundracatalina@gmail.com>
2021-03-28 22:16:28 -0600

    Add annotations about new column and defining new associations to crateProduct table

--- code/api/src/modules/crate/model.js
+++ code/api/src/modules/crate/model.js
@@ -1,3 +1,12 @@
+// Define deliveryDate column that will be added with a migration
+// to the crate table to this file and the DataTypes within the sequelize
+
+// Near the bottom of file add another User.associate to detail
+// the new association that a Crate.hasMany crateProducts
+
+// All of this will help query the database and give access to the FE
+// to display this data
+
 'use strict'
 
 module.exports = function(sequelize, DataTypes) {
