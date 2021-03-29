001d1c769f39fd9e7840a437dd320117f161bc64
Luke Mason <lukjmason@gmail.com>
2021-03-28 18:19:19 -0600

    Add annotations for actions.js

--- code/web/src/modules/user/api/actions.js
+++ code/web/src/modules/user/api/actions.js
@@ -15,6 +15,8 @@ export const LOGOUT = 'AUTH/LOGOUT'
 // Actions
 
 // Set a user after login or using localStorage token
+// This is a method that is invoked only if the axios.post network request returns successfully from
+// the method 'login' below.
 export function setUser(token, user) {
   if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
@@ -27,12 +29,14 @@ export function setUser(token, user) {
 
 // Login a user using credentials
 export function login(userCredentials, isLoading = true) {
+// This first action is dispatched to our user reducer to update the property 'isLoading' to true.
+// This will prompt a loading message while the network request is being returned
   return dispatch => {
     dispatch({
       type: LOGIN_REQUEST,
       isLoading
     })
-
+// This is the fetch request to our database to return the specific user's data that is logging in.
     return axios.post(routeApi, query({
       operation: 'userLogin',
       variables: userCredentials,
@@ -40,23 +44,36 @@ export function login(userCredentials, isLoading = true) {
     }))
       .then(response => {
         let error = ''
-
+// This if statement will trigger if there is an error with logging in the user. This error is returned t
+// to us from the database if this request fails.
         if (response.data.errors && response.data.errors.length > 0) {
           error = response.data.errors[0].message
+// This else if statement will trigger if the database has successfully responded to our user's login
+// request. The authnetication token from the database value is assigned to the variable token, and then
+// user's information from the database is assigned to the variable user.
         } else if (response.data.data.userLogin.token !== '') {
           const token = response.data.data.userLogin.token
           const user = response.data.data.userLogin.user
-
+// The 'setUser' method, which contains the action 'SET_USER', is then dispatched to our user reducer
+// which updates our user state's properties 'isAuthenticated' and 'details'. The 'isAuthenticated'
+// property is updated to true, and the 'details' property is updated with the specific user's information:
+// (email, passowrd, name, etc.)
           dispatch(setUser(token, user))
 
           loginSetUserLocalStorageAndCookie(token, user)
         }
-
+// This method then dispatch's the action 'LOGIN_RESPONSE' to our user reducer which, since the network request
+// was succesful at this point, will update our user state's properties 'isLoading' and 'error'. The 'isLoading'
+// property is updated to false since there is no more information to update at this point, and the 'error'
+// property is updated to an empty string (line 45), unless the user does not exist, then an error message is
+// returned from the api which is set to the 'error' property.
         dispatch({
           type: LOGIN_RESPONSE,
           error
         })
       })
+// This catch method is triggered when the network request above fails to respond. It dispatch's the action 'LOGIN_RESPONSE'
+// which then updates the user state's property 'error' to the string 'Please try again'.
       .catch(error => {
         dispatch({
           type: LOGIN_RESPONSE,
