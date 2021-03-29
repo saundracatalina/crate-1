d36442e96f94082df3154a5fd558e2df32045494
Saundra Catalina <saundracatalina@gmail.com>
2021-03-28 22:23:48 -0600

    Add annotations about seeder files

---
+++ code/api/src/seeders/seeds-notes.js
@@ -0,0 +1,3 @@
+// Add new seeder files to seed new tables (deliveries and crateProducts)
+// Update current seeder files that already exist to have more data in the
+// database to ensure it associations are functioning properly
