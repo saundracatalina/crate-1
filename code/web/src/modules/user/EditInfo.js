2d377dee1109cac1ab616143fd404399b8309168
Luke Mason <lukjmason@gmail.com>
2021-03-30 17:25:54 -0600

    Create overlay effect for EditInfo component

--- code/web/src/modules/user/EditInfo.js
+++ code/web/src/modules/user/EditInfo.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 const EditInfo = () => {
 
   return (
-    <section>
+    <section style={{position: 'fixed', display: 'flex', width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.5)', zIndex: '1'}}>
       <button>Here</button>
     </section>
   )
