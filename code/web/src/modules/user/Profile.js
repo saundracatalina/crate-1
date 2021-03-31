5913dd623535af6addd90556b6908366ec6e3dcc
Luke Mason <lukjmason@gmail.com>
2021-03-31 14:47:35 -0600

    Input placeholders for form inputs

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -35,7 +35,7 @@ const Profile = (props) => {
     <Helmet>
       <title>My Profile - Crate</title>
     </Helmet>
-    {editForm && < EditInfo toggleEditInfoForm={toggleEditInfoForm}/>}
+    {editForm && < EditInfo toggleEditInfoForm={toggleEditInfoForm} userInfo={props.user.details}/>}
 
     {/* Top title bar */}
     <Grid style={{ backgroundColor: grey }}>
