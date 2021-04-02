1a3ae7d239f3bbdd658b32e3a0861d0dcad1f300
Thao Ma <MaThao@gmail.com>
2021-04-02 15:03:18 -0600

    Change setState to have user info value

--- code/web/src/modules/user/EditInfo.js
+++ code/web/src/modules/user/EditInfo.js
@@ -8,10 +8,10 @@ class EditInfo extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      image: '',
-      email: '',
-      shippingAddress: '',
-      description: '',
+      image: props.userInfo.image,
+      email: props.userInfo.email,
+      shippingAddress: props.userInfo.shippingAddress,
+      description: props.userInfo.description,
     }
   }
 
@@ -25,11 +25,11 @@ class EditInfo extends Component {
     return (
       <section style={{position: 'fixed', justifyContent: 'center', alignItems: 'center', display: 'flex', width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.5)', zIndex: '1'}}>
         <form className='edit-info-form' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', width: '60%', height: '50%', backgroundColor: 'white'}}>
-          <Input style={{height: '10%', width: '100%'}} type='text' placeholder={this.props.userInfo.image}/>
-          <Input style={{height: '10%', width: '100%'}} type='text' placeholder={this.props.userInfo.description}/>
-          <Input style={{height: '10%', width: '100%'}} type='text' placeholder={this.props.userInfo.email}/>
-          <Input style={{height: '10%', width: '100%'}} type='text' placeholder={this.props.userInfo.shippingAddress}/>
-          <button className='save-info' onClick={(e) => handleOnClick(e)}>Save</button>
+          <Input style={{height: '10%', width: '100%'}} type='text' />
+          <Input style={{height: '10%', width: '100%'}} type='text' value={this.state.email} />
+          <Input style={{height: '10%', width: '100%'}} type='text' />
+          <Input style={{height: '10%', width: '100%'}} type='text' />
+          <button className='save-info' onClick={(e) => this.handleOnClick(e)}>Save</button>
         </form>
       </section>
     )
