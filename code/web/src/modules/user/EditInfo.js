5313d6ba382b5c2d5a10f4f6ca01d6b9e44f6631
Thao Ma <MaThao@gmail.com>
2021-04-02 14:49:20 -0600

    Change Edit info to class component

--- code/web/src/modules/user/EditInfo.js
+++ code/web/src/modules/user/EditInfo.js
@@ -4,25 +4,36 @@ import { connect } from 'react-redux'
 import Input from '../../ui/input/Input'
 import { editInfoResponse } from './api/actions'
 
-const EditInfo = ({toggleEditInfoForm, userInfo, editInfoResponse}) => {
-
-  const handleOnClick = (e) => {
-    e.preventDefault()
-    toggleEditInfoForm();
-    editInfoResponse();
+class EditInfo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      image: '',
+      email: '',
+      shippingAddress: '',
+      description: '',
+    }
   }
 
-  return (
-    <section style={{position: 'fixed', justifyContent: 'center', alignItems: 'center', display: 'flex', width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.5)', zIndex: '1'}}>
-      <form className='edit-info-form' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', width: '60%', height: '50%', backgroundColor: 'white'}}>
-        <Input style={{height: '10%', width: '100%'}} type='text' placeholder={userInfo.image}/>
-        <Input style={{height: '10%', width: '100%'}} type='text' placeholder={userInfo.description}/>
-        <Input style={{height: '10%', width: '100%'}} type='text' placeholder={userInfo.email}/>
-        <Input style={{height: '10%', width: '100%'}} type='text' placeholder={userInfo.shippingAddress}/>
-        <button className='save-info' onClick={(e) => handleOnClick(e)}>Save</button>
-      </form>
-    </section>
-  )
+  handleOnClick = (e) => {
+    e.preventDefault()
+    this.props.toggleEditInfoForm();
+    this.props.editInfoResponse();
+  }
+
+  render() {
+    return (
+      <section style={{position: 'fixed', justifyContent: 'center', alignItems: 'center', display: 'flex', width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.5)', zIndex: '1'}}>
+        <form className='edit-info-form' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', width: '60%', height: '50%', backgroundColor: 'white'}}>
+          <Input style={{height: '10%', width: '100%'}} type='text' placeholder={this.props.userInfo.image}/>
+          <Input style={{height: '10%', width: '100%'}} type='text' placeholder={this.props.userInfo.description}/>
+          <Input style={{height: '10%', width: '100%'}} type='text' placeholder={this.props.userInfo.email}/>
+          <Input style={{height: '10%', width: '100%'}} type='text' placeholder={this.props.userInfo.shippingAddress}/>
+          <button className='save-info' onClick={(e) => handleOnClick(e)}>Save</button>
+        </form>
+      </section>
+    )
+  }
 }
 
 //Component State
